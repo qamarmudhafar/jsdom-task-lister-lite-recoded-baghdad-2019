59f56f899547883542c73a006df665b9a42cf16c
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-15 06:37:08 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let input= document.getElementById("new-task-description");
+let list=document.getElementById("tasks");
+
+function todolist(event){
+  event.preventDefault();
+  let inputvalue= input.value;
+  let todolist=document.createElement('li');
+  todolist.innerText=inputvalue;
+  list.appendChild(todolist);
+  
+}
+let sub=document.getElementById("btn");
+sub.onclick=todolist;
